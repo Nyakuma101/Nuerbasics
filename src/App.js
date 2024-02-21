3591f430a4bcdfa999d5c32a9c5a395fc6a2162b
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-02-20 21:13:44 -0500

    -

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./Components/SignIn/SignIn";
-import AlphaButtons from "./Components/AlphaButtons";
+import Alphabets from "./Components/Alphabets/Alphabets";
 import Categories from "./Components/Categories/Categories";
 import Numbers from "./Components/Numbers/Numbers";
 import Anatomy from "./Components/Anatomy/Anatomy";
 import Plants from "./Components/Plants/Plants";
+import Home from "./Components/Home/Home";
+import Animals from "./Components/Animals/Animals";
 
 function App() {
   return (
@@ -13,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/SignIn" element={<SignIn />} />
-        <Route path="/AlphaButtons" element={<AlphaButtons />} />
+        <Route path="/Alphabets" element={<Alphabets />} />
         <Route path="/Categories" element={<Categories />} />
         <Route path="/Categories/:Numbers" element={<Numbers />} />
         <Route path="/Categories/:Animals" element={<Animals />} />
