0ad37b897f9bc6e1129be981524b07c7bfb05a48
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-02-21 19:57:10 -0500

    Created the begin Button and the container for the fun facts

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,20 +2,11 @@ import "./Button.scss";
 
 function Button({ text }) {
   return (
-    <>
+    <div>
       <button className="header__upload" type="submit" id="button">
         {text}
       </button>
-      <div class="container">
-        <a href="#" class="button type--A">
-          <div class="button__line"></div>
-          <div class="button__line"></div>
-          <span class="button__text">A</span>
-          <div class="button__drow1"></div>
-          <div class="button__drow2"></div>
-        </a>
-      </div>
-    </>
+    </div>
   );
 }
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,21 @@
+import Button from "../Button/Button";
 import "./Home.scss";
 
 export default function Home() {
   return (
     <div className="home">
-      <div className="home_facts">
-        <h1>Fun Facts</h1>
+      <div className="home__facts">
+        <p className="home__fun">Fun Facts</p>
+        <p className="home__content">
+          this is were i will place the facts inside. i hope it actually fits
+          omg this should work completley.
+        </p>
+        <section className="home__circles"></section>
       </div>
-      <div>
-        <div>
-          <h2>Already have an account</h2>
-        </div>
-        <button>Begin</button>
+
+      <div className="home__account">
+        <p>Already have an account?</p>
+        <Button text="Begin"></Button>
       </div>
     </div>
   );
